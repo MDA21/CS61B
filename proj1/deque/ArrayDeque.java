221e527341abc03dd151ef0e126ca8b5bac949de
zkz <1399443067@qq.com>
2025-07-14 14:35:05 +0800

    proj1A first commit

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,5 +1,50 @@
 package deque;
 
 public class ArrayDeque {
+    private int[] array;
+    private int size;
+    public ArrayDeque() {
+        array = new int[100];
+        size = 0;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void addLast(int item){
+        array[size] = item;
+        size++;
+    }
+
+    public void addFirst(int item) {
+        int[] arr = new int[++size];
+        arr[0] = item;
+        System.arraycopy(array, 0, arr, 1, size);
+        array = arr;
+    }
+
+    public int removeFirst(){
+        int item = array[0];
+        int[] arr = new int[--size];
+        System.arraycopy(array, 1, arr, 0, size);
+        array = arr;
+        return item;
+    }
+
+    public int removeLast(){
+        int item = array[size - 1];
+        size--;
+        return item;
+    }
+
+    public int get(int i){
+        return array[i];
+    }
+
 
 }
