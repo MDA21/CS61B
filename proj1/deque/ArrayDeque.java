85d2de297a5793acd91ea5349e3f879d95b274e7
zkz <1399443067@qq.com>
2025-07-14 16:19:17 +0800

    proj1A second commit

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,50 +1,92 @@
 package deque;
 
-public class ArrayDeque {
-    private int[] array;
+public class ArrayDeque<T> {
+
+    private int nextFirst;
+    private int nextLast;
+    private int capacity;
+    private T[] items;
     private int size;
-    public ArrayDeque() {
-        array = new int[100];
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        this.capacity = items.length;
+        nextFirst = capacity-1;
+        nextLast = 0;
         size = 0;
     }
+    private void resize(int capacity){
+        T[] a = (T[]) new Object[capacity];
+        //由于nextFirst和nextLast的位置不确定，只能一个一个地复制到新的数组中
+        //从nextFirst右边的第一个点开始复制
+        //到nextLast左边的第一个点复制结束
+        for (int i = 1;i <= size;i++)
+            a[i] = items[(++nextFirst) % this.capacity];
+        this.capacity = capacity;
+        //这两个指针指向什么地方已经不重要了
+        nextFirst = 0;
+        nextLast = size+1;
+        items = a;
+    }
+    public void addFirst(T item) {
+        //直接当size等于capacity时调整大小，而不是看两个指针的相对位置
+        if (size == capacity)
+            resize(capacity * 2);
+        items[nextFirst] = item;
+        size++;
+        //nextFirst有可能越界
+        nextFirst = (nextFirst == 0?capacity - 1:nextFirst - 1);
+    }
 
-    public boolean isEmpty(){
+    public void addLast(T item) {
+        if (size == capacity)
+            resize(capacity * 2);
+        items[nextLast] = item;
+        size++;
+        //nextLast有可能越界
+        nextLast = (nextLast+1)%capacity;
+    }
+
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
-    public void addLast(int item){
-        array[size] = item;
-        size++;
+    public void printDeque() {
+        //nextFirst有可能指向最后一个位置
+        for (int i = (nextFirst+1) % capacity; i != nextLast-1; i = (i+1) % capacity)
+            System.out.print(items[i] + " ");
+        System.out.print(items[nextLast - 1]);
     }
 
-    public void addFirst(int item) {
-        int[] arr = new int[++size];
-        arr[0] = item;
-        System.arraycopy(array, 0, arr, 1, size);
-        array = arr;
-    }
-
-    public int removeFirst(){
-        int item = array[0];
-        int[] arr = new int[--size];
-        System.arraycopy(array, 1, arr, 0, size);
-        array = arr;
-        return item;
-    }
-
-    public int removeLast(){
-        int item = array[size - 1];
+    public T removeFirst() {
+        //当数组的内容为空的时候，才无法进行remove操作，而不是取决于nextFirst的位置。
+        if (size == 0)return null;
+        nextFirst = (nextFirst+1) % capacity;
+        T temp = items[nextFirst];
+        items[nextFirst] = null;
         size--;
-        return item;
+        if (capacity>=16 && size < capacity/4)
+            resize(capacity/2);
+        return temp;
     }
 
-    public int get(int i){
-        return array[i];
+    public T removeLast() {
+        if (size == 0)return null;
+        nextLast = (nextLast == 0? capacity-1: nextLast-1);
+        T temp = items[nextLast];
+        items[nextLast] = null;
+        size--;
+        if (capacity>=16 && size<capacity/4)
+            resize(capacity/2);
+        return temp;
     }
 
-
+    public T get(int index) {
+        if (index >= size)
+            return null;
+        return items[(nextFirst + 1 + index) % capacity];
+    }
 }
